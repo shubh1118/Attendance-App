07edc73bb4082456b9e4d7df67fe308f20101cc4
Shubham Chaurasia <82652474+shubh1118@users.noreply.github.com>
2024-05-16 09:35:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Userreport from "./components/Userreport";
+import Homeattendance from "./components/Homeattendance";
+import Adminpage from "./components/Adminpage";
+import Userdetails from "./components/Userdetails";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/*"
+            element={
+              <>
+                <Navbar />
+                <Routes>
+                  <Route path="/userreport" element={<Userreport />} />
+                  <Route path="/homeattendance" element={<Homeattendance />} />
+                  <Route path="/adminpage" element={<Adminpage />} />
+                  <Route path="/userdetails/:userId" element={<Userdetails />} />
+                </Routes>
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
